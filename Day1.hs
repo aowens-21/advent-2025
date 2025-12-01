3eb7d923bfa79db6a1569aba78c7638be7a0e0ee
Alex Owens <aowens5189@gmail.com>
2025-12-01 17:53:03 -0500

    Small cleanup

--- Day1.hs
+++ Day1.hs
@@ -1,35 +1,38 @@
 import Helpers
 
+type ResultPair = (Int, Int)
+
 main = do
     runner "inputs/day1_real.txt" solve1 solve2
 
 solve1 :: [String] -> Int
-solve1 lines =
-    let nums = map parseLine lines
-    in fst $ foldl folder (0, 50) nums
+solve1 lines = computePassword lines compute1 
 
-folder :: (Int, Int) -> Int -> (Int, Int)
-folder (password, rotation) change =
+compute1 :: ResultPair -> Int -> ResultPair
+compute1 (password, rotation) change =
     let newRot = (rotation + change) `mod` 100
     in case newRot of 0 -> (password + 1, newRot)
                       _ -> (password, newRot)
 
-parseLine :: String -> Int
-parseLine line =
-    case head line of   'L' -> - (read num :: Int)
-                        'R' ->  (read num :: Int)
-    where num = tail line
-
 solve2 :: [String] -> Int
-solve2 lines =
-    let nums = map parseLine lines
-    in fst $ foldl folder2 (0, 50) nums
+solve2 lines = computePassword lines compute2 
 
-folder2 :: (Int, Int) -> Int -> (Int, Int)
-folder2 (password, rotation) change =
+compute2 :: ResultPair -> Int -> ResultPair
+compute2 (password, rotation) change =
     let newRotNoMod = (rotation + change)
         newRot = newRotNoMod `mod` 100
     in case newRotNoMod `compare` 0 of GT -> (password + (newRotNoMod `div` 100), newRot)
                                        LT -> case rotation of 0 -> (password + (abs newRotNoMod `div` 100), newRot)
                                                               _ -> (password + 1 + (abs newRotNoMod `div` 100), newRot)
                                        EQ -> (password + 1, 0)
+
+computePassword :: [String] -> (ResultPair -> Int -> ResultPair) -> Int
+computePassword lines computeFunc =
+    let nums = map parseLine lines
+    in fst $ foldl computeFunc (0, 50) nums
+
+parseLine :: String -> Int
+parseLine line =
+    case head line of   'L' -> - (read num :: Int)
+                        'R' ->  (read num :: Int)
+    where num = tail line
