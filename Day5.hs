0fee3457e8b40006edeae88ed9d9afbb388f766f
Alex Owens <aowens5189@gmail.com>
2025-12-06 16:13:06 -0500

    Day 5 part 2

--- Day5.hs
+++ Day5.hs
@@ -2,6 +2,7 @@ import Helpers
 import Data.List
 import Data.List.Split
 import qualified Data.Set as Set
+import Data.Maybe
 
 main = do
     runner "inputs/day5_real.txt" solve1 solve2
@@ -18,7 +19,41 @@ isIdFresh ((start, end):rst) id =
 
 
 solve2 :: [String] -> Int
-solve2 lines = -1
+solve2 lines =
+    let (ranges, _) = parseInput lines
+        rangesWithNoOverlap = collapseRangesUntilSettle ranges
+    in 
+        sum $ map (\(s,e) -> (e - s) + 1) rangesWithNoOverlap
+
+collapseRangesUntilSettle :: [(Int, Int)] -> [(Int, Int)]
+collapseRangesUntilSettle ranges =
+    let collapsed = collapseRanges ranges
+    in
+        if length collapsed == length ranges
+        then collapsed
+        else collapseRangesUntilSettle collapsed
+
+collapseRanges :: [(Int,Int)] -> [(Int, Int)]
+collapseRanges allRanges =
+    nub $ collapseRanges' allRanges 0
+
+collapseRanges' :: [(Int, Int)] -> Int -> [(Int, Int)]
+collapseRanges' ranges idx =
+    if idx >= length ranges then ranges else
+    let range = ranges !! idx
+        firstOverlap = find (\e -> e /= range && isJust (getOverlap range e)) ranges
+    in
+        case firstOverlap of Nothing -> collapseRanges' ranges (idx + 1)
+                             Just range2 -> let rangesWithout = filter (\r -> r /= range && r /= range2) ranges
+                                                overlap = getOverlap range range2
+                                            in
+                                                collapseRanges' (rangesWithout ++ [fromJust overlap]) (idx + 1)
+
+getOverlap :: (Int, Int) -> (Int, Int) -> Maybe (Int, Int)
+getOverlap (s1, e1) (s2, e2) =
+    if s2 > e1 || s1 > e2
+    then Nothing
+    else Just (min s1 s2, max e1 e2)
 
 parseInput :: [String] -> ([(Int,Int)], [Int])
 parseInput lines =
